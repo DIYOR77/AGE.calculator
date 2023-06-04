d028b78c7d9f5de71a9fe15eb8d315c282d8da6e
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-06-04 17:50:50 +0500

    Age.calculator

--- Age.calculator/Program.cs
+++ Age.calculator/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(" Enter your birth  year: ");
+            int birthYear = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(" Enter your birht month : ");
+            int birthMonth = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" Enter your birht  day  :");
+            int birthDay = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            DateTime dateTime1 = new DateTime(birthYear, birthMonth, birthDay);
+
+            int nowYear = DateTime.Now.Year;
+            int nowMonth = DateTime.Now.Month;
+            int nowDay = DateTime.Now.Day;
+            if (nowDay > birthDay)
+            {
+                nowDay -= birthDay;
+            }
+            else
+            {
+                nowDay = nowDay + 30 - birthDay;
+                nowMonth -= 1;
+            }
+            if (nowMonth > birthMonth)
+            {
+                nowMonth -= birthMonth;
+            }
+            else
+            {
+                nowMonth += 12 - birthMonth;
+                nowYear -= 1;
+            }
+            nowYear -= birthYear;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($" Year: {nowYear} Month: {nowMonth} Day: {nowDay}");
         }
     }
 }
