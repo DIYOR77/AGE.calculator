1e1ee06d42e343d0d5789675ed1cc2609d2dd8e9
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-06-04 17:46:26 +0500

    AGE.calculator

---
+++ Age.calculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Age.calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
